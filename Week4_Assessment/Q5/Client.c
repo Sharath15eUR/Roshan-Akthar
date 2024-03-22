02f2f2b24ae6b13a760ab6e9b3c0d0d797107ac0
Roshan Akthar <120296689+roshanakthar-n@users.noreply.github.com>
2024-03-22 16:09:24 +0530

    Add files via upload

---
+++ Week4_Assessment/Q5/Client.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main() {
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // sending connection request
+    if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // sending data
+    char msg1[1024];
+    printf("Enter a string to check if it's a palindrome: ");
+    fgets(msg1, sizeof(msg1), stdin);
+    msg1[strcspn(msg1, "\n")] = 0; // Remove newline character from input
+
+    ssize_t bytesSent = send(clientSocket, msg1, strlen(msg1), 0);
+    if (bytesSent < 0) {
+        perror("Send failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // receiving data
+    char msg2[1024] = {0};
+    ssize_t bytesReceived = recv(clientSocket, msg2, sizeof(msg2), 0);
+    if (bytesReceived < 0) {
+        perror("Receive failed");
+        exit(EXIT_FAILURE);
+    } else if (bytesReceived == 0) {
+        printf("Server disconnected\n");
+    } else {
+        printf("Response from Server: %s\n", msg2);
+    }
+
+    close(clientSocket);
+
+    return 0;
+}
